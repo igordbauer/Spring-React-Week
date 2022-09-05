7e1e7be4ae386fb810565b86fa0a2e4b22a43104
IgorDalpiaz <igor.dalpiaz@networkme.io>
2022-09-05 18:23:21 -0300

    first-component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
- return (
-  <h1>Olá</h1>
- )
+  return (
+    <>
+      <h1>Olá</h1>
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
